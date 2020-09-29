0dc0f0b49575c4ff7eb8227fd4d583280bb8f228
MarchenkoVladislav <vlad25091999@mail.ru>
2020-09-30 02:17:48 +0300

    maven project was created

---
+++ src/main/java/ru/marchenko/regionsdirectory/RegionsDirectoryApplication.java
@@ -0,0 +1,13 @@
+package ru.marchenko.regionsdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionsDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegionsDirectoryApplication.class, args);
+	}
+
+}
